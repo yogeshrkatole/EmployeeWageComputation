befaab4dab1284f81ee5def3d2b67e18e74662cf
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-11-26 11:11:24 +0530

    Calculate monthly Wage

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -1,13 +1,19 @@
 package com.demo;
 
 public class Main {
+    private static final int Working_Days_Per_Month=20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-
         EmployeeAttendance employeeAttendance = new EmployeeAttendance();
         CalculateEmployeeWage calculateEmployeeWage = new CalculateEmployeeWage();
-                int attendance=employeeAttendance.getAttendance();
-             System.out.print("Daily wage " +calculateEmployeeWage.calculateEmployeeWage(attendance));
+        int monthlyWage=0;
+        for(int day=1;day<=Working_Days_Per_Month;day++) {
+            int attendance = employeeAttendance.getAttendance();
+            int dailyWage=calculateEmployeeWage.calculateEmployeeWage(attendance);
+            monthlyWage+=dailyWage;
+            System.out.println("Daily wage " +dailyWage);
+        }
+        System.out.println("Total wage "+monthlyWage);
 
     }
 }
